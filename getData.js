0a953044011bd5e97afafee70892390517ebd2b0
Rudolf <rud.manusachi@gmail.com>
2016-05-04 19:18:41 -0700

    added updateData()

--- getData.js
+++ getData.js
@@ -1,10 +1,19 @@
-var getData = function(fn) {
+(function(){
   var data = [
     ["John",  "Doe",          "02.21.1994", "BackEnd Developer"],
     ["Alex",  "Riberts",      "03.18.1985", "FrontEnd Dev"],
     ["Hakop", "Baghdasaryan", "06.05.1988", "Project Manager"],
     ["Rosa",  "Simonyan",     "01.09.1987",  "Designer"]
   ];
+  localStorage.setItem("data", JSON.stringify(data));
+})();
+
+var getData = function(fn) {
+  var data = localStorage.getItem("data");
 
   fn(data);
+}
+
+var updateData = function(newData){
+  localStorage.setItem("data", newData);
 }
